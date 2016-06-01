4229ee4c6e3366004e706c9b35c8e560261647ea
Ryan Kelley <rpkelley@gmail.com>
2016-06-01 16:49:42 -0600

    Delete start.ps1

--- IaaSDeploy/PsScripts/start.ps1
+++
@@ -1,8 +0,0 @@
-﻿param([string]$port='10933', [string]$env='dev', [string]$role='web-server', [string]$ip='')
-
-$password =  ConvertTo-SecureString "adminP@ssw0rd" -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\adminuser", $password)
-$command = $file = $PSScriptRoot + "\octoInstall.ps1"
-Enable-PSRemoting –force
-Invoke-Command -FilePath $command -Credential $credential -ComputerName $env:COMPUTERNAME -ArgumentList $port, $env, $role, $ip
-Disable-PSRemoting -Force
